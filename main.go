f3d90b9db726c119b7040e90a7e661fb2d81b28f
yangrudan <1549029765@qq.com>
2024-04-02 17:29:56 +0800

    bytes比较

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
+import "bytes"
 import "fmt"
 
 func main() {
-	slFrom := []int{1, 2, 3}
-	slTo := make([]int, 10)
+	a := []byte("go")
+    b := []byte("golang")
+    fmt.Println(bytes.Compare(a, b)) // -1
+    fmt.Println(bytes.Equal(a, b))   // false
 
-	n := copy(slTo, slFrom)
-	fmt.Println(slTo)
-	fmt.Printf("Copied %d elements\n", n) // n == 3
-
-	sl3 := []int{1, 2, 3}
-	sl3 = append(sl3, 4, 5, 6)
-	fmt.Println(sl3)
 }
