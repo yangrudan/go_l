e5664576a2ef024100eaa1f5a9b56741afa71620
yangrudan <1549029765@qq.com>
2024-04-02 15:00:27 +0800

    Gin是一个用Go语言编写的web框架，类似于martini但拥有更好性能的API框架, 由于使用了httprouter，速度提高了近40倍

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "go_l/mypackage"
 
 
 func ping(c *gin.Context) {
+	// c.JSON：返回JSON格式的数据
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
@@ -13,7 +14,11 @@ func ping(c *gin.Context) {
 func main() {
 	println("trans.Pi", trans.Pi)
 
+	// 创建一个默认的路由引擎
 	r := gin.Default()
+
+	// GET：请求方式；/hello：请求的路径
+	// 当客户端以GET方法请求/hello路径时，会执行后面的匿名函数
 	r.GET("/ping", ping)
 	r.Run(":8088")	// listen and serve (默认8080)
 }
