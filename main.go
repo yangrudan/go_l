2d8539087fd5e9012d64ae63a9909bd016410a85
yangrudan <1549029765@qq.com>
2024-04-02 15:16:34 +0800

    GOG

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
-var a = "G"
+var a string
 
 func main() {
-   n()
-   m()
-   n()
-}
-
-func n() {
+   a = "G"
    print(a)
+   f1()
 }
 
-func m() {
-   a = "O"
+func f1() {
+   a := "O"
+   print(a)
+   f2()
+}
+
+func f2() {
    print(a)
 }
