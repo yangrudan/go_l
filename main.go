93049bbc38c4165df576604bd5591b6373d3f05a
yangrudan <1549029765@qq.com>
2024-04-03 17:27:08 +0800

    字符串替换

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
-
 import (
-	"container/list"
 	"fmt"
+	"regexp"
+	"strconv"
 )
-
 func main() {
-	// Create a new list and put some numbers in it.
-	l := list.New()
-	e4 := l.PushBack(4)
-	e1 := l.PushFront(1)
-	l.InsertBefore(3, e4)
-	l.InsertAfter(2, e1)
+	//目标字符串
+	searchIn := "John: 2578.34 William: 4567.23 Steve: 5632.18"
+	pat := "[0-9]+.[0-9]+" //正则
 
-	// Iterate through list and print its contents.
-	for e := l.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value)
+	f := func(s string) string{
+    	v, _ := strconv.ParseFloat(s, 32)
+    	return strconv.FormatFloat(v * 2, 'f', 2, 32)
 	}
 
-	// Output:
-	// 1
-	// 2
-	// 3
-	// 4
+	if ok, _ := regexp.Match(pat, []byte(searchIn)); ok {
+    fmt.Println("Match Found!")
+	}
+
+	re, _ := regexp.Compile(pat)
+	//将匹配到的部分替换为"##.#"
+	str := re.ReplaceAllString(searchIn, "##.#")
+	fmt.Println(str)
+	//参数为函数时
+	str2 := re.ReplaceAllStringFunc(searchIn, f)
+	fmt.Println(str2)
 }
