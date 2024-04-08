2a5a69723463e1875ca2b00916956dbb37287945
yangrudan <1549029765@qq.com>
2024-04-08 14:26:00 +0800

    如果我们不在 main() 中等待，协程会随着程序的结束而消亡。

--- main.go
+++ main.go
@@ -1,41 +1,32 @@
 package main
 
-import "fmt"
-
-type stockPosition struct {
-	ticker     string
-	sharePrice float32
-	count      float32
-}
-
-/* method to determine the value of a stock position */
-func (s stockPosition) getValue() float32 {
-	return s.sharePrice * s.count
-}
-
-type car struct {
-	make  string
-	model string
-	price float32
-}
-
-/* method to determine the value of a car */
-func (c car) getValue() float32 {
-	return c.price
-}
-
-/* contract that defines different things that have value */
-type valuable interface {
-	getValue() float32
-}
-
-func showValue(asset valuable) {
-	fmt.Printf("Value of the asset is %f\n", asset.getValue())
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	var o valuable = stockPosition{"GOOG", 577.20, 4}
-	showValue(o)
-	o = car{"BMW", "M3", 66500}
-	showValue(o)
+	ch := make(chan string)
+
+	go sendData(ch)
+	go getData(ch)
+
+	time.Sleep(1e9)
+}
+
+func sendData(ch chan string) {
+	ch <- "Washington"
+	ch <- "Tripoli"
+	ch <- "London"
+	ch <- "Beijing"
+	ch <- "Tokyo"
+}
+
+func getData(ch chan string) {
+	var input string
+	// time.Sleep(2e9)
+	for {
+		input = <-ch
+		fmt.Printf("%s ", input)
+	}
 }
