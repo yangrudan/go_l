368b67f2a6571d76a3281908e2bddf0cedfc9b8d
yangrudan <1549029765@qq.com>
2024-04-17 15:26:17 +0800

    sync第一版

--- goroutine/hello.go
+++ goroutine/hello.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
+var wg sync.WaitGroup
 
 func hello() {
 	fmt.Println("Hello")
+	wg.Done()  //通知wg将计数器-1
 }
 
-func main() {  //开启一个主goroutine执行main函数
-	go hello()  //开启一个goroutine执行这个函数
+func main() { //开启一个主goroutine执行main函数
+	wg.Add(1)  //计数牌+1
+	go hello() //开启一个goroutine执行这个函数
 	fmt.Println("end")
-	time.Sleep(time.Second)
+	wg.Wait() //等待派发出去的goroutine完成后收兵
 }
